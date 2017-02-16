c07f66890cabb501ed269f6f8e92811008ed1895
Sylvain Lamontagne <sylvain.lamontagne@gmail.com>
2017-02-16 12:40:14 -0500

    Change sync method for dd
    According to https://romanrm.net/dd-benchmark it should be closer to
    real-world task with conv=fdatasync

--- simple-bench.sh
+++ simple-bench.sh
@@ -10,16 +10,17 @@ disk_latency_test() {
     echo "------------------------------"
     if [ "hostx" == "${container_image}x" ]; then
       test_filename="${PWD}/test512kb_zero_${container_image%:*}_${iter}.data"
-      test_cmd="time -p dd if=/dev/zero of=${test_filename} bs=512 count=1000 oflag=dsync "
+      test_cmd="time -p dd if=/dev/zero of=${test_filename} bs=512 count=1000 conv=fdatasync "
       ${prefix_cmd} ${test_cmd}
     else
       test_filename="/mnt/test512kb_zero_$(basename ${container_image%%:*})_${iter}.data"
-      test_cmd="time -p dd if=/dev/zero of=${test_filename} bs=512 count=1000 oflag=dsync "
+      test_cmd="time -p dd if=/dev/zero of=${test_filename} bs=512 count=1000 conv=fdatasync "
       docker_cmd="docker run --rm -v $PWD/:/mnt -w /mnt ${container_image} bash -c "
       ${prefix_cmd} ${docker_cmd} "${test_cmd}"
     fi
     sync -f
     echo "------------------------------"
+    echo
   done
 }
 
@@ -33,16 +34,17 @@ disk_throughput_test() {
     echo "------------------------------"
     if [ "hostx" == "${container_image}x" ]; then
       test_filename="${PWD}/test1G_zero_$(basename ${container_image%%:*})_${iter}.data"
-      test_cmd="time -p dd if=/dev/zero of=${test_filename} bs=1G count=1 oflag=dsync"
+      test_cmd="time -p dd if=/dev/zero of=${test_filename} bs=1G count=1 conv=fdatasync "
       ${prefix_cmd} ${test_cmd}
     else
       test_filename="/mnt/test1G_zero_$(basename ${container_image%%:*})_${iter}.data"
-      test_cmd="time -p dd if=/dev/zero of=${test_filename} bs=1G count=1 oflag=dsync"
+      test_cmd="time -p dd if=/dev/zero of=${test_filename} bs=1G count=1 conv=fdatasync "
       docker_cmd="docker run --rm -v $PWD/:/mnt -w /mnt ${container_image} bash -c "
       ${prefix_cmd} ${docker_cmd} "${test_cmd}"
     fi
     sync -f
     echo "------------------------------"
+    echo
   done
 }
 
@@ -63,6 +65,7 @@ cpu_speed_test() {
     fi
     sync -f
     echo "------------------------------"
+    echo
   done
 
 }
@@ -71,16 +74,19 @@ echo "Testing performance"
 echo "--------------------"
 echo
 echo "Executing Simple CPU Benchmark: 1000 blocks of 1M filled by Zeros sent to md5sum"
+echo
 cpu_speed_test 3
 cpu_speed_test 3 'debian:jessie' 'x86_64'
 cpu_speed_test 3 'multiarch/debian-debootstrap:armhf-jessie' 'ARM'
 
 echo "Executing disk write test for Latency: 1000 blocks of 512B filled by Zeros"
+echo
 disk_latency_test 3
 disk_latency_test 3 'debian:jessie' 'x86_64'
 disk_latency_test 3 'multiarch/debian-debootstrap:armhf-jessie' 'ARM'
 
 echo "Executing disk write test for throughput: 1 block of 1GB filled by Zeros"
+echo
 disk_throughput_test 3
 disk_throughput_test 3 'debian:jessie' 'x86_64'
 disk_throughput_test 3 'multiarch/debian-debootstrap:armhf-jessie' 'ARM'
