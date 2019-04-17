3b543b4ed36a707fdcb83100fcbabc6040111a1c
Sylvain Lamontagne <sylvain.lamontagne@gmail.com>
2019-04-17 16:19:52 -0400

    Add script to find the closest directory matching a version string
    Say you have:
    /app/
      |_ database
            |_ 0.21
                |_ dataset.sql
            |_ 0.22
                |_ dataset.sql
            |_ 0.23
                |_ dataset.sql
    
    Calling the scripts with:
    ```./fuzzy_version.sh 0.23.1-snapshot /app/database```
    
    Will return _/app/database/0.23_
    
    Other examples:
    ```./fuzzy_version.sh 0.24 /app/database```
    
    Will return _/app/database/0.23_

---
+++ fuzzy_version.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+VERSION=$1
+SEARCH_BASEPATH=$2
+
+_find_closest_datadir() {
+    local version="${1:?"You must provide the version to search for"}"
+    local search_basepath="${2:?"You must provide the base directory to search in"}"
+
+    local found_dir=
+    local old_pattern=
+
+    dir_count=$(/bin/ls -1 "${search_basepath}" | wc -l)
+    while [[ -z $found_dir ]] && [[ ${old_pattern} != "${version}*" ]]
+    do
+      found_dir=$(find "$search_basepath" -maxdepth 1 -name "${version}*" | sort -rn |head -n 1)
+      old_pattern="${version}*"
+      if [[ -z $found_dir ]]; then
+        version=${version%[.|-]*}
+      fi
+    done
+    [[ -z $found_dir ]] && exit 2
+    echo "${found_dir}"
+
+}
+
+datadir=$(_find_closest_datadir $VERSION $SEARCH_BASEPATH)
+echo "Closest datadir found for the given version [${VERSION}] in [${SEARCH_BASEPATH}] is : [${datadir}]"
